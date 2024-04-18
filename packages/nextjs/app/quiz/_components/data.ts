a6062958a90b8421dacdc4bbe8b64e8a2a91f618
0xChijioke <chijokeogbuanya@gmail.com>
2024-04-18 22:21:12 +0100

    pin changes

--- packages/nextjs/app/quiz/_components/data.ts
+++ packages/nextjs/app/quiz/_components/data.ts
@@ -4,25 +4,24 @@ import { Readable } from 'stream';
 import { pinata } from '~~/app/common/config';
 
 
-
-
-
 const pinDataWithPinata = async (data: any) => {
   if (!data) {
     throw new Error("Data is empty or invalid.");
   }
 
-  const dataStream = Readable.from([data]);
-  
-  // Pin the concatenated data to IPFS by hash with metadata including a name
+  const bufferData = JSON.stringify(data);
+
+  const dataStream = Readable.from(bufferData);
+
   const options = {
     pinataMetadata: {
       name: "QuizData",
     },
   };
 
-  // Pin the concatenated data to IPFS by hash with metadata
   const { IpfsHash } = await pinata.pinFileToIPFS(dataStream, options);
+
+  console.log(IpfsHash)
 
   return IpfsHash;
   };
